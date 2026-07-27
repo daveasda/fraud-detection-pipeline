22a914ed25a613691c9bb26a7725c29895a41e41
Devni Dharmasooriya <153256670+daveasda@users.noreply.github.com>
2026-07-27 18:51:19 +0530

    Created main function

--- kafka_producer.py
+++ kafka_producer.py
@@ -41,3 +41,35 @@ def send_transaction(transaction:dict):
         print(f"Failed to send transaction: {e}")
         return False
 
+def main():
+    print(f"Starting Kafka producer...")
+    print(f"Bootstrap servers:{KAFKA_BROKER}")
+    print(f"Topic: {TOPIC}")
+    print(f"Generating transactions from {NUM_USERS} users... \n")
+
+    user_ids = [f"USER_{i:04d}" for i in range(1, NUM_USERS + 1)]
+
+    try:
+        batch_num = 0
+        while True:
+            batch_num += 1
+            print(f"\n--- Batch {batch_num} ---")
+
+            for _ in range(TRANSACTIONS_PER_BATCH):
+                user_id = random.choice(user_ids)
+                transaction = generate_transaction(user_id)
+                send_to_kafka(transaction)
+
+            print(f"Sent {TRANSACTIONS_PER_BATCH} transactions. Waiting for {BATCH_DELAY}s...\n")
+            time.sleep(BATCH_DELAY)
+
+    except KeyboardInterrupt:
+        print("Stopping Kafka producer...")
+
+    finally:
+        producer.flush()
+        producer.close()
+        print("Producer closed.")
+
+if __name__ == "__main__":
+    main()
